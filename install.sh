0b48056eb89cd7a893033ac43b4cbbc14966bd51
Ron Chen <ron_chen@arcadyan.com>
2019-12-06 10:48:06 +0800

    revise install.sh -> use cp to instead of ln rename .vim folder to vim

--- install.sh
+++ install.sh
@@ -3,7 +3,8 @@
 echo "The dotfiles path: "
 echo $PWD
 
-ln -s $PWD/oh-my-zsh ~/.oh-my-zsh
+cp -rf $PWD/vim ~/.vim
+cp -rf $PWD/oh-my-zsh ~/.oh-my-zsh
 ln -sf $PWD/zshrc ~/.zshrc
 ln -sf $PWD/gitconfig ~/.gitconfig
 ln -sf $PWD/screenrc ~/.screenrc
